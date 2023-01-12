0b402704f340e8a1670187fdab838b774013c8d0
2yunseong <dbsdltjd3701@naver.com>
2023-01-12 22:50:06 +0900

    test(chap01): 공연비 계산기능 테스트 작성

--- chap01/__test__/index.test.js
+++ chap01/__test__/index.test.js
@@ -1,11 +1,34 @@
-const statement = require('../src/index');
+const { statement, amountFor } = require('../src/index');
 const invoice = require('../src/data/invoice');
 const plays = require('../src/data/plays');
 
-describe('statement test', () => {
+describe('Chap01: Integrate Test', () => {
   test('statement test', () => {
     expect(statement(invoice, plays)).toEqual(
       `청구내역: (고객명: yunseong)\nhamlet: $650.00 (55석)\nAs you like it: $580.00 (35석)\nOthello: $550.00 (45석)\n총액: $1,780.00\n적립 포인트: 52점\n`
     );
+  });
+});
+
+describe('Chap01: Unit Test', () => {
+  test('amountFor: 공연비 계산 기능', () => {
+    const { performances } = invoice;
+    const errorPerf = {
+      playID: 'error',
+      audience: 40,
+    };
+    const errorPlay = {
+      name: 'error',
+      type: 'error',
+    };
+    expect(amountFor(performances[0], plays[performances[0].playID])).toEqual(
+      65000
+    );
+    expect(amountFor(performances[1], plays[performances[1].playID])).toEqual(
+      58000
+    );
+    expect(() => {
+      amountFor(errorPerf, errorPlay);
+    }).toThrow('알수 없는 장르');
   });
 });
